484efa63c62b276cd85dec10a9fa6009983f6818
Manuel <manuelleung93@gmail.com>
2015-12-05 18:05:54 -0500

    Removed DATABASE_PROJECT, APKs

--- DATABASE_PROJECT/webservice/include/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-* Database config variables
-*/
-define("DB_HOST", "hidden-message.me");
-define("DB_USER", "admin_realuser");
-define("DB_PASSWORD", "h4h4y0u5uck");
-define("DB_DATABASE", "admin_webservice");
-?>
