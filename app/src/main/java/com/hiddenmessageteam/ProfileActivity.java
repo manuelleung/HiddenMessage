23d73bf1f548714f778b986b17a76b8733540ac4
Manuel <manuelleung93@gmail.com>
2015-12-05 23:44:40 -0500

    Added profile layout

--- app/src/main/java/com/hiddenmessageteam/ProfileActivity.java
+++ app/src/main/java/com/hiddenmessageteam/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.hiddenmessageteam;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,7 +8,8 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class ProfileActivity extends AppCompatActivity {
 
-    public void onCreate() {
-
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
     }
 }
